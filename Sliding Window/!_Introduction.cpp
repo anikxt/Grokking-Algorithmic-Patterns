0160a2b8f2f20f75be90ab5025dfc40a18a0035d
Aniket <blaze@pop-os.localdomainn>
2021-09-25 16:17:30 +0530

    Added

---
+++ Sliding Window/!_Introduction.cpp
@@ -0,0 +1,47 @@
+#pragma GCC optimize "trapv"
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3", "unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define gc getchar_unlocked
+#define fab(i, a, b) for (int i = a; i <= b; ++i)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << " " << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define f first
+#define se second
+#define all(x) x.begin(), x.end()
+#define clr(x, y) memset(x, y, sizeof(x))
+#define sortall(x) sort(all(x))
+#define vll vector<ll>
+#define vi vector<int>
+#define vvi vector<vi>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define endl '\n'
+#define blaze ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+//=======================
+using namespace std;
+
+const int MOD = 1e9 + 7;
+//=======================
+
+void solve()
+{
+
+    return;
+}
+
+int main()
+{
+    blaze;
+#ifndef ONLINE_JUDGE
+    freopen("/home/blaze/Desktop/Ember/input.txt", "r", stdin);
+    freopen("/home/blaze/Desktop/Ember/output.txt", "w", stdout);
+#endif
+    solve();
+    // cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+    return 0;
+}
---
+++ Sliding Window/readme.md
@@ -0,0 +1,11 @@
+# Probs List
+
+    > ! Introduction
+    A. Maximum Sum Subarray of Size K (easy)
+    B. Smallest Subarray with a given sum (easy)
+    C. Longest Substring with K Distinct Characters (medium)
+    D. Fruits into Baskets (medium)
+    E. No-repeat Substring (hard)
+    F. Longest Substring with Same Letters after Replacement (hard)
+    G. Longest Subarray with Ones after Replacement (hard)
+
